cd59642884db4c73c60de9bf3ca7939f8dcf19e6
Luqman-Ud-Din <luqmanuddinm@gmail.com>
2022-09-18 19:15:41 +0500

    chore: formatted according to PEP 8 guidelines

--- populate_court_connect_start_urls.py
+++ populate_court_connect_start_urls.py
@@ -7,7 +7,8 @@ r = redis.Redis(host='docket-redis', port=6379, db=0)
 r.flushdb()
 
 start_url_t = 'https://courtconnect.courts.delaware.gov/cc/cconnect/ck_public_qry_cpty.cp_personcase_srch_details?' \
-              'backto=P&partial_ind=checked&last_name={}&case_type=ALL&PageNo=1'
+              'partial_ind=checked&last_name={}&case_type=ALL&PageNo=1'
+
 for character in ascii_lowercase:
     start_url = start_url_t.format(character)
     r.lpush('court-connect-crawl:start_urls', start_url)
