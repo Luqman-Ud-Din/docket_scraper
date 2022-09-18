38f83aa0411e9932571a5646b2d7149afe685bec
Luqman-Ud-Din <luqmanuddinm@gmail.com>
2022-09-19 00:16:02 +0500

    chore: declared DocketItem structure

--- docket_scraper/items.py
+++ docket_scraper/items.py
@@ -3,10 +3,41 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+from dataclasses import dataclass, field
+from typing import Union, List
+
 import scrapy
 
 
-class DocketScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+@dataclass
+class PartyItem:
+    end_date: Union[str, None]
+    type: str
+    id: Union[str, None]
+    name: Union[str, None]
+    address: Union[str, None]
+    aliases: Union[str, None]
+    associates: List[str] = field(default_factory=list)
+
+
+@dataclass
+class EntryItem:
+    filing_date: Union[str, None]
+    description: Union[str, None]
+    party: Union[str, None]
+    monetary: Union[str, None]
+    content: Union[str, None]
+
+
+@dataclass
+class DocketItem:
+    url: str
+    id: str
+    start_date: Union[str, None]
+    end_date: Union[str, None]
+    title: str
+    filing_date: str
+    type: str
+    status: str
+    parties: List[PartyItem] = field(default_factory=list)
+    entries: List[EntryItem] = field(default_factory=list)
